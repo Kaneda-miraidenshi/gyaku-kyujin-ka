1cf6fb8725fd7573b2308546d5be266b67753f68
Kaneda-miraidenshi <61171377+Kaneda-miraidenshi@users.noreply.github.com>
2020-03-29 21:42:32 +0900

    jQueryアニメーション追加
    1)headerのカーソルを合わせるとboreder-bottomが表示される
    2)snsのボタンがhover時、大きなサイズになる
    3)スマホサイズの際、メインのテキストを一度隠し、テキストのタイトルを押すと、テキストが表示されるように設定

--- anime.js
+++ anime.js
@@ -7,5 +7,33 @@ $(".list").hover(function(){
 function(){
     $(this).css('border-bottom','0px');
 });
+$('.sns-list a').hover(
+    function(){
 
+$(this).animate({
+    'font-size':'28px'
+},100);
+},function(){
+    $(this).animate({
+        'font-size':'20px'
+    },100);
+
+})
+$('.my').click(function(){
+
+    var $answer = $(this).find('.res-open');
+    if($answer.hasClass('open')) { 
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find("span").text("+");
+      
+    } else {
+      $answer.addClass('open'); 
+      $answer.slideDown();
+      
+        $(this).find("span").text("-");
+     
+      
+    }
+  });
 });
